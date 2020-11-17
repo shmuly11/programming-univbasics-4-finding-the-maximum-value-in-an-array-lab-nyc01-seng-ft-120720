019958593a7db210b97a8db6d0447ac56bd8e2f9
shmuly levitin <shmuly11@gmail.com>
2020-11-17 19:40:46 +0000

    Automatically backed up by Learn

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -1,3 +1,10 @@
 def find_max_value(array)
-  find_max_value(array)
+  count = 0 
+  number = 1 
+  while count < array.length 
+    if array[count] > number
+      number = array[count]
+    end
+  count +=1 
+end
 end
